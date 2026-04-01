285f5c61f9afee363d2a8d313fa0a7240514d3f5
KahnishgaSolaidurairaj <krishnakani19@gmail.com>
2026-04-01 12:12:35 -0500

    skills and projects done

---
+++ src/data/skills.js
@@ -0,0 +1,5 @@
+export default {
+  languages: ["JavaScript", "Python", "C++", "Java", "HTML", "CSS", "Swift", "F#", "SQL", "Go"],
+  frameworks: ["React", "Astro", "Flutter", "Unity 3D"],
+  coursework: ["Data Structures", "Algorithms", "Computer Design"]
+};
