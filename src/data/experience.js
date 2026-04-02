cf416ee7f9e920da8a98ae97bf6b366be3e7c354
KahnishgaSolaidurairaj <krishnakani19@gmail.com>
2026-04-02 12:05:36 -0500

    base experiance dropdown complete

---
+++ src/data/experience.js
@@ -0,0 +1,31 @@
+const experience = [
+  {
+    title: "Sandbox Labratory Assistant",
+    company: "Innovation Center",
+    date: "Feb 2026 - Present5",
+    points: [
+      "Supporting hands-on learning experiences focused on Swift programming and Apple developer tools as part of Apple’s Community Education Initiative",
+      "Collaborating with lab and teaching staff to facilitate immersive, community-focused technology events"
+    ]
+  },
+  {
+    title: "Teaching Assistant",
+    company: "University of Illinois Chicago",
+    date: "Jan 2025 – Present",
+    points: [
+      "Assisted students in debugging C/C++ code and troubleshooting circuit designs to fix wiring issues and improve understanding",
+      "Co-Led hands on lab sessions and collaborated with teaching assistants to explain complex computer design concepts and prepare students for upcoming coursework"
+    ]
+  }, 
+  {
+    title: "Research Assistant - Detecting Emotions in Large Language Models",
+    company: "University of Illinois Chicago",
+    date: "June 2025 - Aug 2025", 
+    points: [
+      "Read academic papers related to LLM exhibition of emotions, and interacted with existing LLM’s to determine whether they behave in a way that exhibits emotions", 
+      "Determined whether the emotions are correlated with some hardware or software signatures"
+    ]
+  }
+];
+
+export default experience;
